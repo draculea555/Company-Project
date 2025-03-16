9189fba1a753767da72a8ad9585d4e06e969f924
Vlad Traciuc <vlad_traciuc@yahoo.com>
2025-03-16 20:06:44 +0200

    Add DB Scripts

---
+++ Scripts/2. CreateUser.sql
@@ -0,0 +1,10 @@
+USE [master]
+GO
+
+CREATE LOGIN [remoteuser] WITH PASSWORD=N'remotePass', DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+ALTER SERVER ROLE [sysadmin] ADD MEMBER [remoteuser]
+GO
+
+
---
+++ Scripts/4. Populate table Companies.sql
@@ -0,0 +1,12 @@
+use [SampleDB]
+
+INSERT INTO Companies ([Name], Exchange, Ticker, Isin, website)
+VALUES
+  ('Apple Inc.', 'NASDAQ', 'AAPL', 'US0378331005', 'http://www.apple.com'),
+  ('British Airways Plc', 'Pink Sheets', 'BAIRY', 'US1104193065', NULL),
+  ('Heineken NV', 'Euronext Amsterdam', 'HEIA', 'NL0000009165', NULL),
+  ('Panasonic Corp', 'Tokyo Stock Exchange', '6752', 'JP3866800000', 'http://www.panasonic.co.jp'),
+  ('Porsche Automobil', 'Deutsche Börse', 'PAH3', 'DE000PAH0038', 'https://www.porsche.com/');
+  
+
+  
